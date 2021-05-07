2c4d18d3a01ba209169e459b05258b60a1edc119
MeiliSimon <simon_meili@sluz.ch>
2021-05-07 18:21:41 +0200

    setup

---
+++ src/main/java/com/example/Energyshop/M151EnergyshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Energyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M151EnergyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M151EnergyshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Energyshop/M151EnergyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Energyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M151EnergyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
